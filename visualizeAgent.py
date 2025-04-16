f0c54e735db466ee8001e32664c284a290476008
Emre Uğur <46279069+Emruur@users.noreply.github.com>
2025-04-16 10:38:09 +0200

    reward no jitter NICE

--- visualizeAgent.py
+++ visualizeAgent.py
@@ -2,7 +2,7 @@ from HexapodEnv import HexapodEnv
 from stable_baselines3 import PPO
 
 env = HexapodEnv(render=True)
-model = PPO.load("reward_stable_low_body/best_model/best_model.zip")
+model = PPO.load("reward_no_jitter/best_model/best_model.zip")
 
 obs, _ = env.reset()
 
